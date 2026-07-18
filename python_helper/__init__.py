b521c706d11b9fe42ec8cea656d404399a15570d
Paweł <pawel.maciejewski95@gmail.com>
2026-07-18 22:10:16 +0200

    feat: export versioned tax and audit helpers

---
+++ python_helper/__init__.py
@@ -0,0 +1,39 @@
+"""Public deterministic helpers for the IP Box wizard."""
+
+from .allocation_audit import (
+    AllocationFinding,
+    allocate_mix_at_cost_date,
+    audit_revenue_allocation,
+    calculate_w_percent,
+    calculate_w_share,
+    reconcile_return_to_ledger,
+)
+from .tax_year_rules import (
+    TaxYearRules,
+    ThermomodernizationLot,
+    apply_thermomodernization_lots,
+    calculate_scale_tax,
+    calculate_tax_for_year,
+    get_tax_year_rules,
+    reconcile_correction_settlement,
+    supported_years,
+    validate_year_amounts,
+)
+
+__all__ = [
+    "AllocationFinding",
+    "TaxYearRules",
+    "ThermomodernizationLot",
+    "allocate_mix_at_cost_date",
+    "apply_thermomodernization_lots",
+    "audit_revenue_allocation",
+    "calculate_scale_tax",
+    "calculate_tax_for_year",
+    "calculate_w_percent",
+    "calculate_w_share",
+    "get_tax_year_rules",
+    "reconcile_correction_settlement",
+    "reconcile_return_to_ledger",
+    "supported_years",
+    "validate_year_amounts",
+]
